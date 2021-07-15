f2de60c3d951946dde9e4667c0002291cada4e07
venkat9940 <52745987+venkat9940@users.noreply.github.com>
2021-07-15 18:16:49 +0530

    Add files via upload

---
+++ authorizationMicroservice/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+  id        INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username	VARCHAR(120) NOT NULL,
+  password	VARCHAR(30) NOT NULL 
+);
+
+INSERT INTO user (username, password) VALUES 
+	('priyanka', 'password'),
+	('deekshith', 'password'),
+	('suneja', 'password'),
+	('tamish', 'password');
+	
+
